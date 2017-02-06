ed78646071ef93f3d64b4befbd04a2658a98e305
Shakalee14 <shakalee14@gmail.com>
2017-02-06 12:46:41 -0800

    Separated routes from server file and created routes for administrators and visitors

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const path = require('path')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const pug = require('pug')
+const admin = require('./routes/admin.js')
+const routes = require('./routes/index.js')
 
 app.set('view engine', 'pug')
+app.use('/', routes)
+app.use('/admin', admin)
 
 app.listen(2782, function(){
   console.log('listening on port 2782')
-})
-
-app.get('/', function(request, response){
-  response.render('index')
 })
