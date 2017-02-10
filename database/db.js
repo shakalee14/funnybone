a5e3dfb799aa922474e48890c8c761931e48605a
Shakalee14 <shakalee14@gmail.com>
2017-02-10 10:40:15 -0800

    Created routes for categories of inventory and updated schema

--- database/db.js
+++ database/db.js
@@ -3,14 +3,14 @@ const env = require('dotenv').config()
 const connectionString = process.env.connectionString
 const db = pgp(connectionString)
 
-const createInventoryItem = function( name, description, available, quantity, img ){
+const createInventoryItem = function( name, description, category, cost, size, available, quantity, img ){
   const sql = `
   INSERT INTO inventory
-    (name, description, available, quantity, img)
+    (name, description, category, cost, size, available, quantity, img)
   values
-    ($1, $2, $3, $4, $5)
+    ($1, $2, $3, $4, $5, $6, $7, $8)
   `
-  return db.any(sql, [name, description, available, quantity, img])
+  return db.any(sql, [name, description, category, cost, size, available, quantity, img])
 }
 
 const displayInventoryItems = function(){
@@ -19,6 +19,21 @@ const displayInventoryItems = function(){
     WHERE available=true
   `
   return db.any(sql)
+}
+
+const displayAllInventoryItems = function(){
+  const sql = `
+    SELECT * FROM inventory
+  `
+  return db.any(sql)
+}
+
+const displayItemsByCategory = function(category){
+  const sql = `
+    SELECT * FROM inventory
+    WHERE category=$1
+  `
+  return db.any(sql, [category])
 }
 
 const getItemDetailsById = function(id){
@@ -70,5 +85,7 @@ module.exports = {
   archiveItem,
   getItemDetailsById,
   updateItem,
-  searchItem
+  searchItem,
+  displayItemsByCategory,
+  displayAllInventoryItems
 }
