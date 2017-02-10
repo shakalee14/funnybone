20802fb1428bc57494dc7b3c66b119a13001cfb7
Shakalee14 <shakalee14@gmail.com>
2017-02-09 17:59:52 -0800

    Added procfile and database connection string

--- database/db.js
+++ database/db.js
@@ -1,5 +1,7 @@
 const pgp = require('pg-promise')()
-const connectionString = 'postgres://localhost:5432/funnybone'
+const CONNECTION_STRING = process.env.NODE_ENV === 'production'
+  ? process.env.DATABASE_URL
+  : `postgres://${process.env.USER}@localhost:5432/funnybone`
 const db = pgp(connectionString)
 
 const createInventoryItem = function( name, description, available, quantity, img ){
