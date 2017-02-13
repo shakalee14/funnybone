bde4fc6d8cf73dc0275b76fea975651272ea08ff
Shakalee14 <shakalee14@gmail.com>
2017-02-13 15:00:55 -0800

    fixed server file

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const bodyParser = require('body-parser')
 const pug = require('pug')
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth').Strategy
-const flash = require('connect-flash')
+const flash    = require('connect-flash')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
-const env = require('dotenv').config()
+
 
 app.use(morgan('dev'))
 app.use(cookieParser())
@@ -17,20 +17,18 @@ app.use(bodyParser())
 
 const admin = require('./routes/admin.js')
 const index = require('./routes/index.js')
-
-app.use(session({ secret: process.env.SECRET }))
+app.use(session({ secret: 'iloveshakashakashaka' }))
 app.use(passport.initialize())
 app.use(passport.session())
 require('./config/passport.js')(passport)
 app.use(flash())
 
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
-
-app.use(express.static(path.join(__dirname, 'public')))
+const GOOGLE_CLIENT_ID = '197695806879-8rtcu8gb9joo2ob3tg28rs2nqf9k8vkm.apps.googleusercontent.com'
+const GOOGLE_CLIENT_SECRET = '_btkQ7Z65MV3YmRtCbn5CfIF'
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)
 app.use('/admin', admin)
