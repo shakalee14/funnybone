6277181b87b9e71c0788d0e56e0d5e089fb44280
Shakalee14 <shakalee14@gmail.com>
2017-02-19 13:30:48 -0800

    Fixed styling issues for client

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
   $('.tabs .tab-links a').on('click', function(event){
-    var currentAttrValue = $(this).attr('href');
+    var currentAttrValue = $(this).attr('href')
 
-    $('.tabs ' + currentAttrValue).show().siblings().hide();
-    $(this).parent('li').addClass('active').siblings().removeClass('active');
-    event.preventDefault();
-  });
+    $('.tabs ' + currentAttrValue).show().siblings().hide()
+    $(this).parent('li').addClass('active').siblings().removeClass('active')
+    event.preventDefault()
+  })
 
   $('.details').on('click', function(event) {
-    event.preventDefault();
+    event.preventDefault()
     var currentItem = $(this).attr('data-id')
 
-    $('body').toggleClass('prevent');
-    $(".inventory-details-modal[data-id=" + currentItem +"]").addClass('active');
+    $('body').toggleClass('prevent')
+    $(".inventory-details-modal[data-id=" + currentItem +"]").addClass('active')
   })
 
   $('.close').on('click', function(event) {
-    event.preventDefault();
-    console.log('hey hey hey')
-    $('.inventory-details-modal').removeClass('active');
+    event.preventDefault()
+    $('.inventory-details-modal').removeClass('active')
   })
-});
+  
+})
